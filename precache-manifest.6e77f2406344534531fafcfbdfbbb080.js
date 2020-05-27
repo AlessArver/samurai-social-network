22c1a1650584fdb4e44b96c014bc528b13afbd9a
Olesya Vyalaya <olesya.vyalaya@arkadium.com>
2020-05-27 23:58:44 +0300

    Updates

---
+++ precache-manifest.6e77f2406344534531fafcfbdfbbb080.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47de93466d7782ab9394bd2f658886af",
+    "url": "/samurai-social-network/index.html"
+  },
+  {
+    "revision": "0d984444aba2e3a5f76e",
+    "url": "/samurai-social-network/static/css/4.8ad5eddb.chunk.css"
+  },
+  {
+    "revision": "fb7c38ef38e18bc10f1e",
+    "url": "/samurai-social-network/static/css/5.08b47ac5.chunk.css"
+  },
+  {
+    "revision": "1bb3b7a182d536d80d8f",
+    "url": "/samurai-social-network/static/css/6.e8348c1c.chunk.css"
+  },
+  {
+    "revision": "286a9c43ad4d5e11141d",
+    "url": "/samurai-social-network/static/css/7.cd0d4606.chunk.css"
+  },
+  {
+    "revision": "7bac5c9d6c01ccd637ce",
+    "url": "/samurai-social-network/static/css/main.09b2bb18.chunk.css"
+  },
+  {
+    "revision": "93836c088e1f9271189d",
+    "url": "/samurai-social-network/static/js/0.c82d8210.chunk.js"
+  },
+  {
+    "revision": "d220fef4f7a03fd38ff2",
+    "url": "/samurai-social-network/static/js/3.38500d71.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/samurai-social-network/static/js/3.38500d71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d984444aba2e3a5f76e",
+    "url": "/samurai-social-network/static/js/4.f9bfe150.chunk.js"
+  },
+  {
+    "revision": "fb7c38ef38e18bc10f1e",
+    "url": "/samurai-social-network/static/js/5.5729508b.chunk.js"
+  },
+  {
+    "revision": "1bb3b7a182d536d80d8f",
+    "url": "/samurai-social-network/static/js/6.ea2780e8.chunk.js"
+  },
+  {
+    "revision": "286a9c43ad4d5e11141d",
+    "url": "/samurai-social-network/static/js/7.522973cd.chunk.js"
+  },
+  {
+    "revision": "7bac5c9d6c01ccd637ce",
+    "url": "/samurai-social-network/static/js/main.7967de70.chunk.js"
+  },
+  {
+    "revision": "ff1695131adba15ad4f5",
+    "url": "/samurai-social-network/static/js/runtime-main.c852bcd1.js"
+  },
+  {
+    "revision": "65cb35c3a4fbb25d39bf4b9ba667555d",
+    "url": "/samurai-social-network/static/media/preloader.65cb35c3.svg"
+  }
+]);
